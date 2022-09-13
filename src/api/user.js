452cbb00b4aec9d6168e96707d6fc426d0149aad
HUAMINO <1906448030@qq.com>
2022-09-13 17:04:55 +0800

    登录页面发送请求

--- src/api/user.js
+++ src/api/user.js
@@ -2,6 +2,11 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
+    method: 'post',
+    // url: '/sys/login',
+    // 模拟服务器转态不为200的错误
+    url: '/sys/login1111',
+    data
   })
 }
 
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,9 +1,37 @@
+import { login } from '@/api/user'
+import { getToken, removeToken, setToken } from '@/utils/auth'
+import { Message } from 'element-ui'
+
 export default {
   namespaced: true,
   state: {
+    token: getToken() // 获取cookie里面的数据
   },
   mutations: {
+    setToken(state, token) {
+      state.token = token
+      // 调用 将数据存储到cookie中
+      setToken(token)
+    },
+    removeToken(state) {
+      state.token = null
+      removeToken()
+    }
   },
   actions: {
+    async login(context, data) {
+      try {
+        // 判断请求是否成功
+        const { data: { success, data: token, message }} = await login(data)
+        // 发送请求，让mutation中的setToken去存储数据
+        if (success) {
+          context.commit('setToken', token)
+        } else {
+          Message.error(message)
+        }
+      } catch (e) {
+        Message.error('服务器出错误')
+      }
+    }
   }
 }

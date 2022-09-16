cc007a54c16679cd3a339ede2b5b5ad69354a89c
HUAMINO <1906448030@qq.com>
2022-09-16 17:26:23 +0800

    左侧导航和管理结构的内容

---
+++ src/router/modules/employees.js
@@ -0,0 +1,12 @@
+import Layout from '@/layout'
+export default {
+  path: '/employees',
+  component: Layout,
+  children: [{
+    path: '',
+    name: 'employees',
+    component: () => import('@/views/employees/index'),
+    meta: { title: '员工', icon: 'people' }
+  }]
+
+}
---
+++ src/views/departments/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <el-card class="tree-card">
+        <!-- 左右布局 方式1 栅格系统 -->
+        <!-- <el-row>
+              <el-col :span="12">左</el-col>
+              <el-col :span="12">右</el-col>
+            </el-row> -->
+        <!-- 左右布局 flex -->
+        <!-- <el-row type="flex">
+          <el-col>左</el-col>
+          <el-col>右</el-col>
+        </el-row> -->
+
+        <el-tree :data="list" :props="{label: 'name'}" default-expand-all>
+          <template v-slot="{ data }">
+            <el-row type="flex" align="middle" style="height: 40px; width: 100%">
+              <el-col>{{ data.name }}</el-col>
+              <el-col type="flex" justify="end">
+                <span>{{ data.manager }}</span>
+                <el-dropdown>
+                  <span class="el-dropdown-link">
+                    操作<i
+                      class="el-icon-arrow-down el-icon--right"
+                    ></i>
+                  </span>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                    <el-dropdown-item>编辑部门</el-dropdown-item>
+                    <el-dropdown-item>删除部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </template>
+        </el-tree>
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Departments',
+  components: {},
+  props: {},
+  data() {
+    return {
+      list: [
+        {
+          name: '总裁办',
+          manager: '曹操',
+          children: [{ name: '董事会', manager: '曹丕' }]
+        },
+        { name: '行政部', manager: '刘备' },
+        { name: '人事部', manager: '孙权' }
+      ]
+    }
+  },
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.tree-card {
+  padding: 30px 140px;
+  font-size: 14px;
+}
+.el-dropdown-link {
+  margin-left: 10px;
+}
+</style>
